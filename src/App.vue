9ead29c9483bf81b1b6f0bc9c9d74b948672379d
romnoelp <petracorta.rmnl@gmail.com>
2025-05-07 19:43:34 +0800

    Feature: Added Installation and Introduction component.
    Separated the components for easier modification.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,34 @@
 <script setup>
-import Navbar from './components/Navbar.vue';
+import Navbar from './components/Navbar.vue'
+import Introduction from './components/Introduction.vue'
+import Installation from './components/Installation.vue'
 </script>
 
 <template>
-<header>
-    <Navbar/>
+  <header>
+    <Navbar />
+    <img src="./assets/arch.png" alt="header-background" class="img-fluid" />
   </header>
+  <main class="p-4">
+    <section class="d-flex flex-column">
+      <h1 class="display-3 text-center">Arch Linux + Hyprland installation</h1>
+    </section>
+    <hr />
+    <article>
+      <Introduction />
+    </article>
+
+    <section>
+      <Installation />
+    </section>
+  </main>
 </template>
+
+<style scoped>
+.img-fluid {
+  width: 100%;
+  max-height: 60vh;
+  height: auto;
+  object-fit: cover;
+}
+</style>
---
+++ src/components/Introduction.vue
@@ -0,0 +1,23 @@
+<template>
+  <p class="text-justify lh-sm lead">
+    <span class="ps-4">Welcome</span> to my corner of the web. This guide will walk you through the
+    process of installing Arch Linux and setting up a customized Hyprland desktop environment,
+    potentially incorporating Project Hyde. I understand that an Arch installation can seem complex,
+    especially when aiming for a specific configuration. However, I'll provide step-by-step
+    instructions and explanations to help you navigate the process. Consider this a straightforward
+    guide to building your own personalized Arch Linux system. Let's get started! So you can now
+    also say:
+  </p>
+  <figure class="text-end">
+    <blockquote class="blockquote">
+      <p><em>I use arch btw.</em></p>
+    </blockquote>
+    <figcaption class="blockquote-footer">Some random bloke on the internet</figcaption>
+  </figure>
+</template>
+
+<style scoped>
+.text-justify {
+  text-align: justify;
+}
+</style>
